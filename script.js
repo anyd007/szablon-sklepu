77cb27a36c3a5e270ec4320dcbdacfcd270070e8
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-09 18:39:51 +0100

    kocowe ustawienia

---
+++ script.js
@@ -0,0 +1,53 @@
+const nav = document.querySelector("nav");
+const rightAsiad = document.querySelector("#right-asiad");
+const footer = document.querySelector("footer");
+const usrInput = document.querySelector("#usrInput");
+const senBTN = document.querySelector("#sendBTN");
+
+const openPhoto = (e) => {
+  if (e.target.matches("#cat1")) {
+    window.open("/photos/cat1.jpg");
+  }
+  if (e.target.matches("#cat2")) {
+    window.open("/photos/cat2.jpg");
+  }
+  if (e.target.matches("#cat3")) {
+    window.open("/photos/cat3.jpg");
+  }
+};
+const openLinks = (e) => {
+  if (e.target.matches(".youtube")) {
+    window.open("https://www.youtube.com/watch?v=hY7m5jjJ9mM/");
+  }
+  if (e.target.matches(".facebook")) {
+    window.open("https://www.facebook.com/catsthegame/");
+  }
+  if (e.target.matches(".instagram")) {
+    window.open("https://www.instagram.com/cats_of_instagram/");
+  }
+  if (e.target.matches(".twetter")) {
+    window.open("https://twitter.com/cats");
+  }
+  if (e.target.matches(".adoption")) {
+    window.open("https://fundacjakot.pl/koty-do-adopcji/");
+  }
+};
+const sendMsg = () => {
+  if (usrInput.value == "") {
+    senBTN.disabled = true
+    usrInput.style.backgroundColor = "tomato";
+    usrInput.value = "to pole jest obowiązkowe...";
+  } else {
+    usrInput.style.backgroundColor = "white";
+    usrInput.value = "";
+  }
+};
+usrInput.onclick = () => {
+    senBTN.disabled = false
+  usrInput.style.backgroundColor = "white";
+  usrInput.value = "";
+};
+
+rightAsiad.addEventListener("click", openPhoto);
+footer.addEventListener("click", openLinks);
+senBTN.addEventListener("click", sendMsg);
